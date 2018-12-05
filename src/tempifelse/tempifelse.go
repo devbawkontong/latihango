9336fdfe7d803875c5068317eaf39feb6ff29a2c
Developer bawangkontong <hendyandiawan@gmail.com>
2018-12-05 15:05:27 +0700

    update utk scanner float

--- src/tempifelse/tempifelse.go
+++ src/tempifelse/tempifelse.go
@@ -1,9 +1,12 @@
 package main
 
-import "fmt"
-import "bufio"
-import "os"
-//import "strconv"
+import (
+	"strings"
+	"fmt"
+	"bufio"
+	"os"
+	"strconv"
+)
 
 func main(){
 	reader := bufio.NewReader(os.Stdin)
@@ -11,10 +14,10 @@ func main(){
 	text, _ := reader.ReadString('\n')
 
 	fmt.Print(text)
-	//var point, _ = strconv.ParseFloat(text, 64)
-	var float = 100.23
-	fmt.Printf("point is %f \n", float)
-	if percent := float / 100; percent >= 100 {
+	var point, _ = strconv.ParseFloat(strings.TrimSpace(text), 64)
+
+	fmt.Printf("point is %f \n", point)
+	if percent := point / 100; percent >= 100 {
 		fmt.Printf("%.2f%s perfect!\n", percent, "%")
 	} else if percent >= 70 {
 		fmt.Printf("%.2f%s good\n", percent, "%")
