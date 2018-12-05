8f6c133adb2728927d2fa938d903a20e2bc42f97
Developer bawangkontong <hendyandiawan@gmail.com>
2018-12-05 17:32:00 +0700

    latihan switch lagi

--- src/testswitch/latswitch.go
+++ src/testswitch/latswitch.go
@@ -13,12 +13,13 @@ func main(){
 	fmt.Print("Enter number: ")
 	text, _ := reader.ReadString('\n')
 
-	var point, _ = strconv.ParseInt(strings.TrimSpace(text), 16, 32)
+	var point, _ = strconv.ParseInt(strings.TrimSpace(text), 10, 0)
+	fmt.Printf("point is %d\n",point)
 
 	switch point {
-	case 8:
+	case 10:
 		fmt.Println("perfect")
-	case 7:
+	case 7,8,9:
 		fmt.Println("awesome")
 	default:
 		fmt.Println("not bad")
