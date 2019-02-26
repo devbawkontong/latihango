4682e887823e0a1c35b04a116601da1b48e3f953
Developer bawangkontong <hendyandiawan@gmail.com>
2019-02-26 14:56:49 +0700

    latihan 26feb19

--- src/2019-02/29-kombinasi-slice-map/kombinasi-sm.go
+++ src/2019-02/29-kombinasi-slice-map/kombinasi-sm.go
@@ -6,10 +6,18 @@ func main() {
 	var cities = []map[string]string{
 		{"city": "surabaya", "province": "jawa timur"},
 		{"city": "jakarta", "province": "dki jaya"},
-		{"city": "medan"},
+		{"city": "medan", "province": "sumatera utara"},
 	}
 	for _, city := range cities {
-		fmt.Println(city["province"])
+		fmt.Println(city["city"], city["province"])
 	}
-
+	fmt.Println()
+	var data = []map[string]string{
+		{"name": "chicken blue", "gender": "male", "color": "brown"},
+		{"address": "mangga street", "id": "k001"},
+		{"community": "chicken lovers"},
+	}
+	for _, dt := range data {
+		fmt.Println(dt)
+	}
 }
