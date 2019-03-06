e9fba3002de37a491eb86f594a8fd4313bfa4106
Developer bawangkontong <hendyandiawan@gmail.com>
2019-03-06 17:09:52 +0700

    sore...

--- src/2019-03-part-2/56-acc-mod-struct/library/library.go
+++ src/2019-03-part-2/56-acc-mod-struct/library/library.go
@@ -1,6 +1,20 @@
 package library
 
-type Student struct {
+import "fmt"
+
+/*
+type Student = struct {
 	Name string
 	Age  int
+}
+*/
+var Student = struct {
+	Name string
+	Age  int
+}{}
+
+func init() {
+	Student.Name = "Star-Lord"
+	Student.Age = 35
+	fmt.Println("automatic generate data")
 }
--- src/2019-03-part-2/56-acc-mod-struct/main.go
+++ src/2019-03-part-2/56-acc-mod-struct/main.go
@@ -1,10 +1,13 @@
 package main
 
 import "2019-03-part-2/56-acc-mod-struct/library"
-import "fmt"
+import f "fmt"
 
 func main() {
-	var p = library.Student{Name: "Doel", Age: 19}
-	fmt.Println(p.Name)
-	fmt.Println(p.Age)
+	//var p = library.Student{Name: "Doel", Age: 19}
+	//f.Println(p.Name)
+	//f.Println(p.Age)
+	f.Println(library.Student.Name)
+	f.Println(library.Student.Age)
+	sayHi("golang..")
 }
