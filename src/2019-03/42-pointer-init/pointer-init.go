36860bd8969bc6e39b807f1529accfc5955af01c
Developer bawangkontong <hendyandiawan@gmail.com>
2019-03-14 11:43:55 +0700

    refreah pointer

--- src/2019-03/42-pointer-init/pointer-init.go
+++ src/2019-03/42-pointer-init/pointer-init.go
@@ -12,4 +12,17 @@ func main() {
 	fmt.Println("numA addr :", &numA)
 	fmt.Println("numB value :", *numB)
 	fmt.Println("numB addr :", numB)
+
+	numA = 5
+	fmt.Println("numA value :", numA)
+	fmt.Println("numB value :", *numB)
+
+	i, j := 42, 2701
+	p := &i         // menunjuk ke i
+	fmt.Println(*p) // baca i lewat pointer
+	*p = 21         // set i lewat pointer
+	fmt.Println(i)  // lihat nilai terbaru dari i
+	p = &j          // p menunjuk ke j
+	*p = *p / 37    // bagi nilai j lewat pointer
+	fmt.Println(j)  // lihat nilai terbaru dari j
 }
