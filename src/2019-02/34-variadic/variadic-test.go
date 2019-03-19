5076f1484fcb3992579ad43ff904fc6d42725447
Developer bawangkontong <hendyandiawan@gmail.com>
2019-03-19 11:27:58 +0700

    edit variadic

--- src/2019-02/34-variadic/variadic-test.go
+++ src/2019-02/34-variadic/variadic-test.go
@@ -11,9 +11,22 @@ func ratarata(numbers ...int) float64 {
 	return avg
 }
 
+func ratarata2(numbers []int) float64 {
+	var total int
+	for _, angka := range numbers {
+		total += angka
+	}
+	var avg = float64(total) / float64(len(numbers))
+	return avg
+}
+
 func main() {
 	var numbers = []int{2, 3, 4, 5, 9}
 	var rata = ratarata(numbers...)
 	var msg = fmt.Sprintf("rata-rata %.2f", rata)
 	fmt.Println(msg)
+
+	var rata2 = ratarata2(numbers)
+	var msg2 = fmt.Sprintf("rata-rata %.2f", rata2)
+	fmt.Println(msg2)
 }
