5f38528a8e0c2a69f4d1e31791883de4ddb9c435
Developer bawangkontong <hendyandiawan@gmail.com>
2019-03-20 14:52:13 +0700

    latihan siang

---
+++ src/2019-03-part-3/72-goroutine/goroutine-init.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+import "runtime"
+
+func cetak(batas int, pesan string) {
+	for i := 0; i < batas; i++ {
+		fmt.Println((i + 1), pesan)
+	}
+}
+
+func main() {
+	runtime.GOMAXPROCS(4)
+
+	go cetak(5, "hola")
+	cetak(5, "halo")
+
+	var input, input2, input3 string
+	fmt.Scanln(&input, &input2, &input3)
+	fmt.Println(input)
+	fmt.Println(input2)
+	fmt.Println(input3)
+}
